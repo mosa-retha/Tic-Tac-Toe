c03aa8ab18138f72420e03c4fea7de008d5d15a8
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-10-10 16:18:11 +0200

    clearing redundancy

--- GameSystem.py
+++ GameSystem.py
@@ -7,7 +7,7 @@ class MainGame(Key):
         self.key = {
             7: 0, 8: 1, 9: 2, 4: 3, 5: 4, 6: 5, 1: 6, 2: 7, 3: 8
         }
-        self.turn = True
+        self.turn = False
 
     def turn_x_o(self):
         self.turn = not self.turn
@@ -67,6 +67,9 @@ class MainGame(Key):
             elif n == "O":
                 print("Player 2 Wins!")
             return True
+        elif " " not in tic_list:
+            print("Draw!")
+            return True
         else:
             return False
 
@@ -91,83 +94,13 @@ class MainGame(Key):
             return False
 
 # --- below is for printing the ascii art of 'X' or 'O' to the designated position ---
-    def one(self, num):
-        if self.tic[0] == "X" or self.tic[0] == "O":
-            if self.tic[0] == "O":
+
+    def ascii_print(self, num, list_index):
+        if self.tic[list_index] == "O" or self.tic[list_index] == "X":
+            if self.tic[list_index] == "O":
                 return self.putOInSquares(num)
-            elif self.tic[0] == "X":
+            elif self.tic[list_index] == "X":
                 return self.putXInSquares(num)
         else:
             return self.putEInSquares(num)
 
-    def two(self, num):
-        if self.tic[1] == "X" or self.tic[1] == "O":
-            if self.tic[1] == "O":
-                return self.putOInSquares(num)
-            elif self.tic[1] == "X":
-                return self.putXInSquares(num)
-        else:
-            return self.putEInSquares(num)
-
-    def three(self, num):
-        if self.tic[2] == "X" or self.tic[2] == "O":
-            if self.tic[2] == "O":
-                return self.putOInSquares(num)
-            elif self.tic[2] == "X":
-                return self.putXInSquares(num)
-        else:
-            return self.putEInSquares(num)
-
-    def four(self, num):
-        if self.tic[3] == "X" or self.tic[3] == "O":
-            if self.tic[3] == "O":
-                return self.putOInSquares(num)
-            elif self.tic[3] == "X":
-                return self.putXInSquares(num)
-        else:
-            return self.putEInSquares(num)
-
-    def five(self, num):
-        if self.tic[4] == "X" or self.tic[4] == "O":
-            if self.tic[4] == "O":
-                return self.putOInSquares(num)
-            elif self.tic[4] == "X":
-                return self.putXInSquares(num)
-        else:
-            return self.putEInSquares(num)
-
-    def six(self, num):
-        if self.tic[5] == "X" or self.tic[5] == "O":
-            if self.tic[5] == "O":
-                return self.putOInSquares(num)
-            elif self.tic[5] == "X":
-                return self.putXInSquares(num)
-        else:
-            return self.putEInSquares(num)
-
-    def seven(self, num):
-        if self.tic[6] == "X" or self.tic[6] == "O":
-            if self.tic[6] == "O":
-                return self.putOInSquares(num)
-            elif self.tic[6] == "X":
-                return self.putXInSquares(num)
-        else:
-            return self.putEInSquares(num)
-
-    def eight(self, num):
-        if self.tic[7] == "X" or self.tic[7] == "O":
-            if self.tic[7] == "O":
-                return self.putOInSquares(num)
-            elif self.tic[7] == "X":
-                return self.putXInSquares(num)
-        else:
-            return self.putEInSquares(num)
-
-    def nine(self, num):
-        if self.tic[8] == "X" or self.tic[8] == "O":
-            if self.tic[8] == "O":
-                return self.putOInSquares(num)
-            elif self.tic[8] == "X":
-                return self.putXInSquares(num)
-        else:
-            return self.putEInSquares(num)
--- main.py
+++ main.py
@@ -7,29 +7,29 @@ from Minimax import MiniMax
 # prints ascii art step by step
 def f():
     f = f"""
-    {game.one(0)}|{game.two(0)}|{game.three(0)}
-    {game.one(1)}|{game.two(1)}|{game.three(1)}
-    {game.one(2)}|{game.two(2)}|{game.three(2)}
-    {game.one(3)}|{game.two(3)}|{game.three(3)}
-    {game.one(4)}|{game.two(4)}|{game.three(4)}
-    {game.one(5)}|{game.two(5)}|{game.three(5)}
-    {game.one(6)}|{game.two(6)}|{game.three(6)}
+    {game.ascii_print(0, 0)}|{game.ascii_print(0,1)}|{game.ascii_print(0, 2)}
+    {game.ascii_print(1, 0)}|{game.ascii_print(1,1)}|{game.ascii_print(1, 2)}
+    {game.ascii_print(2, 0)}|{game.ascii_print(2,1)}|{game.ascii_print(2, 2)}
+    {game.ascii_print(3, 0)}|{game.ascii_print(3,1)}|{game.ascii_print(3, 2)}
+    {game.ascii_print(4, 0)}|{game.ascii_print(4,1)}|{game.ascii_print(4, 2)}
+    {game.ascii_print(5, 0)}|{game.ascii_print(5,1)}|{game.ascii_print(5, 2)}
+    {game.ascii_print(6, 0)}|{game.ascii_print(6,1)}|{game.ascii_print(6, 2)}
     ________________________________________
-    {game.four(0)}|{game.five(0)}|{game.six(0)}
-    {game.four(1)}|{game.five(1)}|{game.six(1)}
-    {game.four(2)}|{game.five(2)}|{game.six(2)}
-    {game.four(3)}|{game.five(3)}|{game.six(3)}
-    {game.four(4)}|{game.five(4)}|{game.six(4)}
-    {game.four(5)}|{game.five(5)}|{game.six(5)}
-    {game.four(6)}|{game.five(6)}|{game.six(6)}
+    {game.ascii_print(0,3)}|{game.ascii_print(0,4)}|{game.ascii_print(0,5)}
+    {game.ascii_print(1,3)}|{game.ascii_print(1,4)}|{game.ascii_print(1,5)}
+    {game.ascii_print(2,3)}|{game.ascii_print(2,4)}|{game.ascii_print(2,5)}
+    {game.ascii_print(3,3)}|{game.ascii_print(3,4)}|{game.ascii_print(3,5)}
+    {game.ascii_print(4,3)}|{game.ascii_print(4,4)}|{game.ascii_print(4,5)}
+    {game.ascii_print(5,3)}|{game.ascii_print(5,4)}|{game.ascii_print(5,5)}
+    {game.ascii_print(6,3)}|{game.ascii_print(6,4)}|{game.ascii_print(6,5)}
     ________________________________________
-    {game.seven(0)}|{game.eight(0)}|{game.nine(0)}
-    {game.seven(1)}|{game.eight(1)}|{game.nine(1)}
-    {game.seven(2)}|{game.eight(2)}|{game.nine(2)}
-    {game.seven(3)}|{game.eight(3)}|{game.nine(3)}
-    {game.seven(4)}|{game.eight(4)}|{game.nine(4)}
-    {game.seven(5)}|{game.eight(5)}|{game.nine(5)}
-    {game.seven(6)}|{game.eight(6)}|{game.nine(6)}
+    {game.ascii_print(0,6)}|{game.ascii_print(0,7)}|{game.ascii_print(0,8)}
+    {game.ascii_print(1,6)}|{game.ascii_print(1,7)}|{game.ascii_print(1,8)}
+    {game.ascii_print(2,6)}|{game.ascii_print(2,7)}|{game.ascii_print(2,8)}
+    {game.ascii_print(3,6)}|{game.ascii_print(3,7)}|{game.ascii_print(3,8)}
+    {game.ascii_print(4,6)}|{game.ascii_print(4,7)}|{game.ascii_print(4,8)}
+    {game.ascii_print(5,6)}|{game.ascii_print(5,7)}|{game.ascii_print(5,8)}
+    {game.ascii_print(6,6)}|{game.ascii_print(6,7)}|{game.ascii_print(6,8)}
     """
     return f
 
