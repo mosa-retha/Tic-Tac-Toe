a76b6db7573e9ba9bf8f30dae12ba8ee3f6772ae
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-10-10 15:47:10 +0200

    import minimax code clean up

--- Minimax.py
+++ Minimax.py
@@ -1,3 +1,5 @@
+from random import randint, choice
+
 from GameSystem import MainGame
 """
 [_,_,_
@@ -6,20 +8,18 @@ _,_,_]
 """
 
 class MiniMax(MainGame):
-    def __init__(self, tac_toe_list=[]):
+    def __init__(self):
         super().__init__()
-        self.tac = tac_toe_list
         self.mini = -1
         self.max = +1
         self.tie = 0
 
 
 
-    def minimax(self, count=0):
-        if self.win_check("O"):
-            return self.mini
-        if self.win_check("X"):
-            return self.max
+
+
+
+
 
 
 
