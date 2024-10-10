1c8678dc99be761292c929ac97846891effd9529
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-10-10 15:48:38 +0200

    initial implementation of minimax logic

--- Minimax.py
+++ Minimax.py
@@ -16,6 +16,24 @@ class MiniMax(MainGame):
 
 
 
+    def minimax(self, tic_list,count=0):
+        turn = self.turn_x_o()
+        print(tic_list)
+        available_choices = [index for index, value in enumerate(tic_list) if value == " "]
+
+        if self.win_check("O", tic_list):
+            return self.mini, count
+        if self.win_check("X", tic_list):
+            return self.max, count
+        if " " not in tic_list:
+            return self.tie, count
+
+        choice_ = choice(available_choices)
+
+        if self.taken(turn,num=choice_, tic_list=tic_list, ai=True):
+            count += 1
+            return self.minimax(tic_list,count)
+
 
 
 
