e2f43111dc44f0862d710c3d321e4ab0986f0e66
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-11-09 14:41:26 +0200

    added play with cpu and player function plus a play type function

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ def print_ascii_art():
 
 
 game = MainGame()
-
-
 tic = game.tic
 
 def print_welcome_ascii():
@@ -34,29 +32,83 @@ __  _  __ ____ |  |   ____  ____   _____   ____   _/  |_  ____   _/  |_|__| ____
 
 """
 
-print(print_ascii_art())
-game_started = True
 
-# the game loop
-while game_started:
-    turn = "X"
 
-    if not game.taken(turn, tic):
+
+
+def play_with_cpu():
+    game_started = True
+    while game_started:
+        turn = "X"
+
+        if not game.taken(turn, tic):
+            print(print_ascii_art())
+            continue
+
         print(print_ascii_art())
-        continue
+        winner = game.win_check(turn, tic)
+        if winner or winner is None:
+            break
 
-    if game.win_check(turn, tic):
-        break
+        turn = "O"
+        node = MiniMaxNode(tic)
+        node.insert(tic, turn)
+        num = node.minimax(turn)
 
-    turn = "O"
-    node = MiniMaxNode(tic)
-    node.insert(tic, turn)
-    num = node.minimax(turn)
+        if not game.taken(turn, tic, num=num[2], cpu=True):
+            print(print_ascii_art())
+            continue
 
-    if not game.taken(turn,tic, num=num[2] , ai=True):
         print(print_ascii_art())
-        continue
-    print(print_ascii_art())
-    if game.win_check(turn, tic):
-        break
+        winner = game.win_check(turn, tic)
+        if winner or winner is None:
+            break
+
+
+def play_with_player():
+    game_started = True
+    while game_started:
+        turn = "X"
+        if not game.taken(turn, tic):
+            print(print_ascii_art())
+            continue
+
+        print(print_ascii_art())
+        winner = game.win_check(turn, tic)
+        if winner or winner is None:
+            break
+
+        turn = "O"
+        if not game.taken(turn, tic):
+            print(print_ascii_art())
+            continue
+
+        print(print_ascii_art())
+        winner = game.win_check(turn, tic)
+        if winner or winner is None:
+            break
+
+
+def play_type():
+    while True:
+        play = input("Do you want to play with CPU or Player? (CPU/Player): ").lower()
+        print(print_ascii_art())
+        if play == "cpu":
+            play_with_cpu()
+            break
+        elif play == "player":
+            play_with_player()
+            break
+        else:
+            print("Please enter a valid input")
+
+
+
+if __name__ == "__main__":
+    print(print_welcome_ascii())
+    play_type()
+    print("Game Over")
+    print("Thanks for playing")
+    print("Bye!")
+
 
